904a8f90f566e5d6b88aec959952d3cb46855b7e
Matt <mattsully01@gmail.com>
2020-09-09 14:08:14 -0600

    revamping this whole project

--- App/JS/index.js
+++ App/JS/index.js
@@ -1,38 +1,24 @@
 const skills = [
   "Javascript",
-  "HTML5",
+  "HTML",
   "CSS",
-  "SQL",
-  "MongoDb",
   "React",
-  "NodeJs",
-  "QA Testing",
-  "Rest/Api",
-  "SEO",
-  "Agile Mehodologies",
-  "Express",
   "Java",
-  "Bootstrap",
-  "Materialize",
-  "Handlebars",
-  "EJS",
-  "Firebase",
+  "Agile Development",
 ];
 
-
-document.addEventListener("DOMContentLoaded", function(event) {     
+document.addEventListener("DOMContentLoaded", function (event) {
   for (l = 0; l < skills.length; l++) {
     const listItem = document.createElement("li");
     const text = skills[l];
-    
+
     console.log(listItem);
-    
+
     listItem.value = text;
     listItem.innerHTML = text;
 
     listItem.classList.add("skills-list-item");
 
-    document.getElementById('skillsList').append(listItem);  
-  
+    document.getElementById("skillsList").append(listItem);
   }
 });
